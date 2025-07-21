da46382ca8f019991bc51563458c1a6808104ea0
Navneet Agarwal <agrawalnavneet960@gmail.com>
2025-07-21 21:55:42 +0530

    added routes and component header

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from "./components/ui/button"; 
-
+import Hero from './components/custom/Hero.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2> Welcome to our Trivago</h2>
-      <button> submit</button>
+    <Hero/>
     </>
   )
 }
