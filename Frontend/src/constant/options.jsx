73243890ee50261c80c9b08e02ae8effe5170f23
Navneet Agarwal <agrawalnavneet960@gmail.com>
2025-07-22 11:00:10 +0530

    added feautures in index.jsx and options.jsx

--- Frontend/src/constant/options.jsx
+++ Frontend/src/constant/options.jsx
@@ -1,1 +1,51 @@
-//
+export const SelectTravel = [
+  {
+    id: 1,
+    title: 'Just Me',
+    desc: 'A soul in search of adventure',
+    icon: '🧍',
+    people: '1'
+  },
+  {
+    id: 2,
+    title: 'A Couple',
+    desc: 'Two travelers in tandem',
+    icon: '👫',
+    people: '2 People'
+  },
+  {
+    id: 3,
+    title: 'Small Group',
+    desc: 'Fun with friends or family',
+    icon: '👨‍👩‍👧‍👦',
+    people: '3-5 People'
+  },
+  {
+    id: 4,
+    title: 'Large Group',
+    desc: 'Group travel vibes',
+    icon: '🧑‍🤝‍🧑',
+    people: '6+ People'
+  }
+];
+
+export const SelectBugetOption = [
+  {
+    id: 1,
+    title: 'Cheap',
+    desc: 'Stay conscious of costs',
+    icon: '💰'
+  },
+  {
+    id: 2,
+    title: 'Moderate',
+    desc: 'Keep costs on the average side',
+    icon: '💵'
+  },
+  {
+    id: 3,
+    title: 'Luxury',
+    desc: 'Premium experiences and fine dining',
+    icon: '💎'
+  }
+];
--- Frontend/src/create-trip/index.jsx
+++ Frontend/src/create-trip/index.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
+import { Button } from "@/components/ui/Button"; // if file is Button.jsx
+import { Input } from "@/components/ui/Input"; // if file is Input.
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { SelectBugetOption, SelectTravel } from '@/constant/options';
 
+
+
+// Assuming you have this component
 function CreateTrip() {
   const [place, setPlace] = useState();
 
@@ -12,26 +18,62 @@ function CreateTrip() {
       </p>
 
       <div className='mt-20 flex flex-col gap-10'>
-        <div >
-        <h2 className='text-xl my-2 font-medium'>What is your destination of choice?</h2>
-        <GooglePlacesAutocomplete
-          apiKey={import.meta.env.VITE_GOOGLE_API_KEY}
-          selectProps={{
-            value: place,
-            onChange: (v) => {
-              setPlace(v);
-              console.log(v);
-            },
-          }}
-        />
-      </div></div>
+        <div>
+          <h2 className='text-xl my-2 font-medium'>What is your destination of choice?</h2>
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_API_KEY}
+            selectProps={{
+              value: place,
+              onChange: (v) => {
+                setPlace(v);
+                console.log(v);
+              },
+            }}
+          />
+        </div>
 
-<div>
+        <div>
           <h2 className='text-xl my-2 font-medium'>How many days do you want to spend on this trip?</h2>
           <input placeholder={'Ex.3'} type="number" className='border-2 border-gray-300 rounded-md p-2 w-full' />
-</div>
+        </div>
 
+        <div>
+          <h2 className='text-xl my-2 font-medium'>What is Your Budget? <br />
+            <p>The budget is exclusively allocated for activities and dining purposes.</p>
+          </h2>
 
+          <div className='grid grid-cols-3 gap-5 mt-5'>
+            {SelectBugetOption.map((item, index) => (
+              <div
+                key={index} className='p-4 border cursor-pointer rounded-lg hover:shadow-lg'>
+                <h2 className='text-4xl'>{item.icon}</h2>
+                <h2 className='font-bold text-lg'>{item.title}</h2>
+                <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h2 className='text-xl my-2 font-medium'>Who do you plan on travelling with on your next adventure?</h2>
+
+          <div className='grid grid-cols-3 gap-5 mt-5'>
+            {SelectTravel.map((item, index) => (
+              <div
+                key={index} className='p-4 border cursor-pointer rounded-lg hover:shadow-lg'>
+                <h2 className='text-4xl'>{item.icon}</h2>
+                <h2 className='font-bold text-lg'>{item.title}</h2>
+                <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className='mt-10 mb-10 justify-end flex'>
+      <Button> 
+        Generate Trip</Button>
+         </div>
     </div>
   );
 }
