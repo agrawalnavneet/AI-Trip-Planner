cadd03a9101a69a09bb771ecec5ba64a493a6c43
Navneet Agarwal <agrawalnavneet960@gmail.com>
2025-07-22 06:13:21 +0530

    added google map api

--- Frontend/src/components/custom/Hero.jsx
+++ Frontend/src/components/custom/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button } from '../ui/button'
-
+import { Link } from 'react-router-dom'
 function Hero() {
   return (
     <div className='flex flex-col items-center mx-56 gap-9'>
@@ -9,8 +9,10 @@ function Hero() {
 
 
 <p className='text-xl text-gray-500 text-center'> It is your personal travel companion, designed to help you plan, organize, and enjoy your journeys with ease. From creating custom itineraries and tracking destinations to budgeting and storing travel memories, it puts everything in one place. </p>
-<Button> Get Started, It's free</Button>
 
+<Link to ={'/create-trip'}>
+<Button> Get Started, It's free</Button>
+ </Link>
 
     </div>
   )
--- Frontend/src/create-trip/index.jsx
+++ Frontend/src/create-trip/index.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 function CreateTrip() {
+  const [place, setPlace] = useState();
+
   return (
-    <div>
-      <h1>Create Trip</h1>
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
+      <h2 className='font-bold text-3xl'>Customize your travel experience</h2>
+      <p className='mt-3 text-gray-500 text-xl'>
+        Just provide some basic information, and our trip planner will create a personalized travel plan for you—tailored to your interests, budget, and schedule.
+      </p>
+
+      <div className='mt-20'>
+        <h2 className='text-xl my-2 font-medium'>What is your destination of choice?</h2>
+        <GooglePlacesAutocomplete
+          apiKey={import.meta.env.VITE_GOOGLE_API_KEY}
+          selectProps={{
+            value: place,
+            onChange: (v) => {
+              setPlace(v);
+              console.log(v);
+            },
+          }}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateTrip
+export default CreateTrip;
