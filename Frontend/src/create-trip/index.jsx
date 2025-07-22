359ddad07fc4ce4ce3795f9f652bf3ca31ed50b9
Navneet Agarwal <agrawalnavneet960@gmail.com>
2025-07-22 11:04:43 +0530

    added feautures in index.jsx and options.jsx

--- Frontend/src/create-trip/index.jsx
+++ Frontend/src/create-trip/index.jsx
@@ -12,7 +12,7 @@ function CreateTrip() {
 
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
-      <h2 className='font-bold text-3xl'>Customize your travel experience</h2>
+      <h2 className='font-bold text-3xl'>Customize your travel experience 🏕️🌴</h2>
       <p className='mt-3 text-gray-500 text-xl'>
         Just provide some basic information, and our trip planner will create a personalized travel plan for you—tailored to your interests, budget, and schedule.
       </p>
